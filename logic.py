c6603c545a2842aa3d85cfe1327dc3589e1c3fb9
Daria Rzepka <dariarz0110@gmail.com>
2019-02-13 14:59:49 +0100

    funcionality to save new question in logic.py

---
+++ logic.py
@@ -0,0 +1,44 @@
+import data_manager
+import time
+
+def sort_questios(sort_by, order):
+    question_data = data_manager.sort_questions(sort_by, order)
+    return question_data
+
+def get_questions():
+    questions = data_manager.get_question_data()
+    return questions
+
+def new_question_id():
+    last_id = data_manager.last_question_id()
+    return last_id + 1
+
+
+def date_generator():
+    time_stamp = time.time()
+    return int(time_stamp)
+
+def new_question(title, message, image = None):
+    question = {}
+    question['id'] = new_question_id()
+    question['submission_time'] = date_generator()
+    question['title'] = title
+    question['message'] = message
+    question['image'] = image
+    data_manager.save_new_question(question)
+
+
+
+#ANSWERS
+
+def new_answer_id()
+    data_manager.
+
+def new_answer(title, message, image = None, ):
+    answer = {}
+    answer['id'] = new_question_id()
+    answer['submission_time'] = date_generator()
+    answer['title'] = title
+    answer['message'] = message
+    question['question_id'] = image
+    data_manager.save_new_answer(answer)
