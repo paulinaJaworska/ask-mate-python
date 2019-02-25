2e35afbd5c83f09d5c1f7078c69b8ec90a155289
kMatejak <ksmatejak@gmail.com>
2019-02-25 12:30:15 +0100

    fix/clean code in server.py

--- server.py
+++ server.py
@@ -10,37 +10,49 @@ app.static_folder = 'static'
 @app.route('/')
 def route_latest_questions():
     latest_questions = logic.get_latest_questions()
-    return render_template('list.html', questions=latest_questions)
+
+    return render_template('list.html',
+                           questions=latest_questions)
 
 
 @app.route('/list')
 def index():
     questions = logic.get_questions()
-    return render_template('list.html', questions=questions)
+
+    return render_template('list.html',
+                           questions=questions)
 
 
 @app.route('/question/<question_id>')
 def question_page(question_id):
     question = logic.get_question_by_id(str(question_id))
     answers = logic.get_answer_by_question_id(str(question_id))
-    return render_template('question.html', question=question, answers=answers)
+
+    return render_template('question.html',
+                           question=question,
+                           answers=answers)
 
 
 @app.route('/question/<question_id>/edit', methods=['GET'])
 def route_edit_question(question_id):
     question_details = logic.get_question_by_id(question_id)
-    return render_template('new_question.html', question=question_details, edition=True)
+
+    return render_template('new_question.html',
+                           question=question_details,
+                           edition=True)
 
 
 @app.route('/question/<question_id>/edit', methods=['POST'])
 def edit_question(question_id):
     new_data = request.form.to_dict()
     logic.edit_question(new_data)
+
     return redirect('/question/<question_id>')
 
 
 @app.route("/add-question", methods=['GET'])
 def route_new_question():
+
     return render_template('new_question.html')
 
 
@@ -49,6 +61,7 @@ def new_question():
     form = request.form.to_dict()
     question = logic.new_question(form)
     question_id = question[id]
+
     return redirect("/question/%s" % question_id)
 
 
@@ -56,7 +69,10 @@ def new_question():
 def route_new_answer(question_id):
     question = logic.get_question_by_id(str(question_id))
     answers = logic.get_answer_by_question_id(str(question_id))
-    return render_template('new_answer.html', question=question, answers=answers)
+
+    return render_template('new_answer.html',
+                           question=question,
+                           answers=answers)
 
 
 @app.route("/<question_id>/new-answer", methods=['POST'])
@@ -64,12 +80,14 @@ def new_answer(question_id):
     # save it to file
     form = request.form.to_dict()
     logic.new_answer(form, question_id)
+
     return redirect("/question/%s" % question_id)
 
 
 @app.route('/question/<question_id>/delete')
 def delete_question(question_id):
     logic.delete_question(question_id)
+
     return redirect('/')
 
 
@@ -78,7 +96,9 @@ def sorted_condition():
     sort_by = request.args.get('condition')
     order = request.args.get('order')
     questions = logic.sort_questions(sort_by, order)
-    return render_template('list.html', questions=questions)
+
+    return render_template('list.html',
+                           questions=questions)
 
 
 if __name__ == "__main__":
