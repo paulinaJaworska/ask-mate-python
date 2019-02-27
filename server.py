6b4dd39c11136382ad682bbe04519066911c612d
kMatejak <ksmatejak@gmail.com>
2019-02-27 09:03:30 +0100

    fixed syntax in server.py

--- server.py
+++ server.py
@@ -157,7 +157,7 @@ def route_new_question_comment():
     return render_template('question_new_comment.html')
 
 
-@app.route('/question/<question_id>/new-comment', methods = ['POST'])
+@app.route('/question/<question_id>/new-comment', methods=['POST'])
 def new_question_comment(question_id):
     comment = request.form.to_dict()
     logic.add_new_question_comment(comment, question_id)
@@ -170,7 +170,7 @@ def route_new_answer_comment():
     return render_template('answer_new_comment.html')
 
 
-@app.route('/answer/<answer_id>/new-comment', methods = ['POST'])
+@app.route('/answer/<answer_id>/new-comment', methods=['POST'])
 def new_answer_comment(answer_id):
     comment = request.form.to_dict()
     question_id = logic.get_question_id_by_answer_id(answer_id)
@@ -179,7 +179,7 @@ def new_answer_comment(answer_id):
     return redirect("/question/%s" % question_id)
 
 
-@app.route('/comments/<comment_id>/edit', methods= ['GET'])
+@app.route('/comments/<comment_id>/edit', methods=['GET'])
 def route_edit_comment(comment_id):
     comment = logic.get_comment_by_id(comment_id)
 
@@ -187,7 +187,7 @@ def route_edit_comment(comment_id):
                            comment=comment)
 
 
-@app.route('/comments/<comment_id>/edit', methods= ['POST'])
+@app.route('/comments/<comment_id>/edit', methods=['POST'])
 def edit_comment(comment_id):
     edited_count = logic.get_edited_count() + 1
 
@@ -201,7 +201,7 @@ def edit_comment(comment_id):
 
 # TAGS
 
-@app.route('/question/<question_id>/new-tag', methods='GET')
+@app.route('/question/<question_id>/new-tag', methods=['GET'])
 def route_new_tag(question_id):
     tags = logic.get_question_tags_by_question_id(question_id)
 
@@ -209,7 +209,7 @@ def route_new_tag(question_id):
                            tags=tags)
 
 
-@app.route('/question/<question_id>/new-tag', methods='POST')
+@app.route('/question/<question_id>/new-tag', methods=['POST'])
 def new_tag(question_id):
     tag = request.form.to_dict()
     logic.add_new_tag(tag, question_id)
