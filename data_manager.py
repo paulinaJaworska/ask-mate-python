ed60643c452e9e67a340444825406974e8527bc7
Daria Rzepka <dariarz0110@gmail.com>
2019-02-28 14:51:15 +0100

    getting all ids connected with question funcionaliy

--- data_manager.py
+++ data_manager.py
@@ -247,3 +247,20 @@ def delete_question_tag(cursor, question_id, tag_id):
     cursor.execute("""DELETE FROM qestion_tag
                       WHERE question_id = %(question_id)s 
                         AND tag_id = %(tag_id)s;""", question_id, tag_id)
+
+@db_connection.connection_handler
+def get_tags_id_realted_to_question(cursor, question_id):
+    cursor.execute("""SELECT tag_id FROM question_tag
+                        WHERE question_id =%s""", question_id)
+    tags_id = cursor.fetchall()
+    return tags_id
+
+@db_connection.connection_handler
+def get_ids_related_to_question(cursor, question_id):
+    cursor.execute("""SELECT question_tag.tag_id, answer.id as answer_id, comment.id AS comment_id 
+    from question_tag 
+    inner join answer on question_tag.question_id=answer.question_id 
+    inner join comment  on question_tag.question_id = comment.question_id
+    WHERE answer.question_id =%s""", question_id)
+    data = cursor.fetchall()
+    return data
